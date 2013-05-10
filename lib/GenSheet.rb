0b3bd74a58a6afa281b34859e908dbfec1e1f89e
R-Y <k_nazuki@hkg.odn.ne.jp>
2013-05-10 10:50:13 +0900

    WIP

--- lib/GenSheet.rb
+++ lib/GenSheet.rb
@@ -13,57 +13,60 @@ class GenSheet
     formats = workbook.instance_variable_get('@formats')
     worksheets = workbook.instance_variable_get('@worksheets')
 
+    # 出力シート準備、元シート分解
     outsheets = Array.new
     originalsheets = Array.new
     names = Array.new
-
-    # 出力シート準備、元シート分解
-    @roo.each_with_pagename do |name, sheet|
-      # FIXME MySheetが上書きされる…
-      outsheets << @outbook.add_worksheet(name)
-      originalsheets << sheet
-      names << name
-    end
+    pre_sheet(outsheets, originalsheets, names)
 
     # mergeしてあるセルのセット
-    for i in 0..worksheets.size - 1
-      merge = worksheets[i].instance_variable_get('@merged_cells')
-      if merge != nil
-        set_mergedcell(outsheets[i], merge)
-      end
-    end
+    merges = Array.new
+    set_mergedcell(worksheets, outsheets, merges)
 
     # シートのセット
-    fonts = @roo.instance_variable_get('@fonts')
-    for i in 0..originalsheets.size - 1
-      set_sheet(outsheets[i], originalsheets[i], names[i], merge, fonts)
-    end
+    set_sheet(outsheets, originalsheets, names, merges)
 
-    # 出力
-    for i in 0..originalsheets.size - 1
-      originalsheets[i].parse do |row|
-        puts row
-      end
-    end
+    # # 出力
+    # for i in 0..originalsheets.size - 1
+      # originalsheets[i].parse do |row|
+        # puts row
+      # end
+    # end
 
     @outbook.close
   end
 
-  def set_mergedcell(outsheet, merge)
-    format = @outbook.add_format(:align => 'merge')
-    for j in 0..merge.size - 1
-      outsheet.merge_range(merge[j][0], merge[j][2], merge[j][1], merge[j][3], '', format)
+  def pre_sheet(outsheets, originalsheets, names)
+    @roo.each_with_pagename do |name, sheet|
+      # FIXME MySheetが上書きされる…
+      outsheets << @outbook.add_worksheet(name)
+      originalsheets << sheet.clone
+      names << name
     end
   end
 
-  def set_sheet(outsheet, originalsheet, name, merge, fonts)
+  def set_mergedcell(worksheets, outsheets, merges)
+    for i in 0..worksheets.size - 1
+      merges << worksheets[i].instance_variable_get('@merged_cells')
+      if merges[i] != nil && merges[i] != []
+        format = @outbook.add_format(:align => 'merge')
+        for j in 0..merges.size - 1
+          temp = merges[i]
+          outsheets[i].merge_range(temp[j][0], temp[j][2], temp[j][1], temp[j][3], '', format)
+        end
+      end
+    end
+  end
+
+  def set_sheet(outsheets, originalsheets, names, merges)
     #@getformat = @formats[(x + 1) * (y + 1) - 1]   # fontもborderも入ってるけどもインデックスがわからない
-    originalsheet.each_with_index do |row, y|
-      row.each_with_index do |cell, x|
-        # XXX なぜかMySheetの情報が消えるので一時的に挿入
-        break if !fonts.key?(name)
-        font = fonts[name][[y + 1, x + 1]]
-        set_cell(outsheet, cell, x, y, font, merge)
+    fonts = @roo.instance_variable_get('@fonts')
+    for i in 0..originalsheets.size - 1
+      originalsheets[i].each_with_index do |row, y|
+        row.each_with_index do |cell, x|
+          font = fonts[names[i]][[y + 1, x + 1]]
+          set_cell(outsheets[i], cell, x, y, font, merges[i])
+        end
       end
     end
   end
@@ -90,15 +93,15 @@ class GenSheet
       #:top => 1,         # border
       #:left => 1,        #
       #:right => 1,       #
-      :color => font.color,
-      :italic => font.italic ? 1 : 0,
-      :font => font.name,
-      :outline => font.outline,
-      :shadow => font.shadow,
-      :size => font.size,
-      :strikeout => font.strikeout,
-      :underline => font.underline == :none ? 0 : 1,  # アンダーラインの種類は4つだけどとりあえず
-      :bold => font.weight > 400 ? 1 : 0              # weightが普通だと400、boldだと700になるようなのでとりあえず
+      :color => font.instance_variable_get('@color'),
+      :italic => font.instance_variable_get('@italic') ? 1 : 0,
+      :font => font.instance_variable_get('@name'),
+      :outline => font.instance_variable_get('@outline'),
+      :shadow => font.instance_variable_get('@shadow'),
+      :size => font.instance_variable_get('@size'),
+      :strikeout => font.instance_variable_get('@strikeout'),
+      :underline => font..instance_variable_get('@underline') == :none ? 0 : 1,  # アンダーラインの種類は4つだけどとりあえず
+      :bold => font.instance_variable_get('@weight') > 400 ? 1 : 0              # weightが普通だと400、boldだと700になるようなのでとりあえず
       #:encoding => font.encoding,                    #
       #:escapement => font.escapement,                # fontにまとめて入っていたけど
       #:family => font.family,                        # どれに対応するのか..
