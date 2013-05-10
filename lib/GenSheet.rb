0620024699b7d258a12b972c7efe5cee1941e608
R-Y <k_nazuki@hkg.odn.ne.jp>
2013-05-10 11:31:45 +0900

    WIP

--- lib/GenSheet.rb
+++ lib/GenSheet.rb
@@ -7,7 +7,7 @@ class GenSheet
   @roo
 
   def to_xls(filename)
-    @outbook = WriteExcel.new(filename)
+    @outbook_xls = WriteExcel.new(filename)
 
     workbook = @roo.instance_variable_get('@workbook')
     formats = workbook.instance_variable_get('@formats')
@@ -17,14 +17,14 @@ class GenSheet
     outsheets = Array.new
     originalsheets = Array.new
     names = Array.new
-    pre_sheet(outsheets, originalsheets, names)
+    pre_sheet_xls(outsheets, originalsheets, names)
 
     # mergeしてあるセルのセット
     merges = Array.new
-    set_mergedcell(worksheets, outsheets, merges)
+    set_mergedcell_sheet(worksheets, outsheets, merges)
 
     # シートのセット
-    set_sheet(outsheets, originalsheets, names, merges)
+    set_sheet_xls(outsheets, originalsheets, names, merges)
 
     # # 出力
     # for i in 0..originalsheets.size - 1
@@ -33,45 +33,48 @@ class GenSheet
       # end
     # end
 
-    @outbook.close
+    @outbook_xls.close
   end
 
-  def pre_sheet(outsheets, originalsheets, names)
+  def pre_sheet_xls(outsheets, originalsheets, names)
     @roo.each_with_pagename do |name, sheet|
-      # FIXME MySheetが上書きされる…
-      outsheets << @outbook.add_worksheet(name)
+      outsheets << @outbook_xls.add_worksheet(name)
       originalsheets << sheet.clone
       names << name
     end
   end
 
-  def set_mergedcell(worksheets, outsheets, merges)
+  def set_mergedcell_sheet(worksheets, outsheets, merges)
     for i in 0..worksheets.size - 1
       merges << worksheets[i].instance_variable_get('@merged_cells')
       if merges[i] != nil && merges[i] != []
-        format = @outbook.add_format(:align => 'merge')
-        for j in 0..merges.size - 1
-          temp = merges[i]
-          outsheets[i].merge_range(temp[j][0], temp[j][2], temp[j][1], temp[j][3], '', format)
-        end
+        set_mergedcell(outsheets[i], merges[i])
       end
     end
   end
 
-  def set_sheet(outsheets, originalsheets, names, merges)
+  def set_mergedcell(outsheet, merge)
+    format = @outbook_xls.add_format(:align => 'merge')
+    for j in 0..merge.size - 1
+      outsheet.merge_range(merge[j][0], merge[j][2], merge[j][1], merge[j][3], '', format)
+    end
+  end
+
+  def set_sheet_xls(outsheets, originalsheets, names, merges)
     #@getformat = @formats[(x + 1) * (y + 1) - 1]   # fontもborderも入ってるけどもインデックスがわからない
     fonts = @roo.instance_variable_get('@fonts')
     for i in 0..originalsheets.size - 1
       originalsheets[i].each_with_index do |row, y|
         row.each_with_index do |cell, x|
           font = fonts[names[i]][[y + 1, x + 1]]
-          set_cell(outsheets[i], cell, x, y, font, merges[i])
+          set_cell_xls(outsheets[i], cell, x, y, font, merges[i])
         end
       end
     end
   end
 
-  def set_cell(outsheet, cell, x, y, font, merge)
+  def set_cell_xls(outsheet, cell, x, y, font, merge)
+    # cellが空の場合は何もしない
     return if cell == nil
 
     # 各フォントフォーマットのセット
@@ -88,7 +91,7 @@ class GenSheet
   end
 
   def set_format(font)
-    format = @outbook.add_format(
+    format = @outbook_xls.add_format(
       #:bottom => 1,      #
       #:top => 1,         # border
       #:left => 1,        #
@@ -118,29 +121,44 @@ class GenSheet
 
   def to_ods(filename)
     puts "converting sheet"
-    outbook = ODF::SpreadSheet.new
+    @outbook_ods = ODF::SpreadSheet.new
 
+    tables = Array.new
+    sheets = Array.new
+    pre_sheet_ods(tables, sheets)
+
+    @outbook_ods.write_to filename
+  end
+
+  def pre_sheet_ods(tables, sheets)
     @roo.each_with_pagename do |name, sheet|
       # シート作成
-      ob_table = outbook.table name
-
-      sheet.each_with_index do |row, y|
-        # 行作成
-        ob_row = ob_table.row
-
-        row.each_with_index do |cell, x|
-          # スタイル作成
-          outbook.style 'font-style', :family => :cell do
-            #property :text, 'font-weight' => 'bold', 'color' => '#ff0000'
-          end
-
-          # セル作成、スタイル適用
-          ob_cell = ob_row.cell(cell, :style => 'font-style')
-        end
-      end
+      tables << (@outbook_ods.table name)
+      sheets << sheet.clone
     end
 
-    outbook.write_to filename
+    set_sheet_ods(tables, sheets)
+  end
+
+  def set_sheet_ods(tables, sheets)
+    for i in 0..sheets.size - 1
+      sheets[i].each_with_index do |row, y|
+        # 行作成
+        ob_row = tables[i].row
+        set_cell_ods(row, ob_row)
+      end
+    end
+  end
+
+  def set_cell_ods(row, ob_row)
+    row.each_with_index do |cell, x|
+      # スタイル作成
+      @outbook_ods.style 'font-style', :family => :cell do
+        #property :text, 'font-weight' => 'bold', 'color' => '#ff0000'
+      end
+      # セル作成、スタイル適用
+      ob_cell = ob_row.cell(cell, :style => 'font-style')
+    end
   end
 
   def initialize(roo)
