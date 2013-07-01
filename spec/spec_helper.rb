0c5f7902dff40634293c85c927c735b3490f5609
影月 零 <zero@genshin.org>
2013-07-02 01:59:12 +0900

    fixed syntax error in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 require_relative '../lib/GenSheet.rb'
 
 def open_xls
-  xls = GenSheet::Spreadsheet.open('./spec/files/template.xls')
+  xls = GenSheet.open('./spec/files/template.xls')
   xls.inspect
   return xls
 end
 
 def open_ods
-  ods = GenSheet::Spreadsheet.open('./spec/files/template.ods')
+  ods = GenSheet.open('./spec/files/template.ods')
   ods.inspect
   return ods
 end
